0d246765647d5a42d8bcb12a7ae92feb3c5c3d56
Haoyue Wang <hwang521@umd.edu>
2012-07-16 14:10:44 -0700

    Change DNS

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,8 @@
 require 'bundler/capistrano'
 
+set :normalize_asset_timestamps, false
 set :user, 'deploy'
-set :domain, 'barkbest.myapp.com'
+set :domain, 'barkbest.declaring.it'
 set :java_home, ENV['JAVA_HOME']#"/usr/lib/jvm/java-6-openjdk"
 #set(:rake) { "JAVA_HOME=#{java_home} GEM_HOME=#{gem_home} RAILS_ENV=#{rails_env} /usr/bin/env rake" }
 set :application, "myapp"
@@ -19,14 +20,14 @@ set :checkout, 'export'
 set :scm_passphrase, "umdterpsU1"  # The deploy user's password
 set :deploy_to, "/home/#{user}/#{domain}"
 set :use_sudo, false
- 
+
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
- 
+
 role :web, domain                        # Your HTTP server, Apache/etc
 role :app, domain                          # This may be the same as your `Web` server
 role :db,  domain, :primary => true # This is where Rails migrations will run
- 
+
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
